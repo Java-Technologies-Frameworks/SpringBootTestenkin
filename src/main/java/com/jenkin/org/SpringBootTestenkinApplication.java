5b784f20b333c833f7c30097beebe103c486b8cd
Prasanna Kumar Jena <classicprasana@gmail.com>
2019-08-15 17:34:05 +0530

    first jenkin commit fromgit bash

---
+++ src/main/java/com/jenkin/org/SpringBootTestenkinApplication.java
@@ -0,0 +1,26 @@
+package com.jenkin.org;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestenkinApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringBootTestenkinApplication.class);
+	
+	public static void main(String[] args) {
+		logger.info("inside main class execution....");
+		SpringApplication.run(SpringBootTestenkinApplication.class, args);
+	}
+	
+	@PostConstruct
+	public void init()
+	{
+		logger.info("inside init method execution ");
+	}
+
+}
---
+++ src/test/java/com/jenkin/org/SpringBootTestenkinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jenkin.org;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTestenkinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
